04f4c2528d827f5713f6970073a08bea45162988
Nhan vi <vinhandtvt@gmail.com>
2021-01-15 15:33:30 +0700

    add style to user component

--- src/app/client/footer/footer.component.ts
+++ src/app/client/footer/footer.component.ts
@@ -1,4 +1,6 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  subscribeForm: FormGroup
+  contactForm: FormGroup
+  constructor() {
+    this.subscribeForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email])
+    });
+    this.contactForm = new FormGroup({
+      hoTen: new FormControl(null, [Validators.required, Validators.pattern("^[a-zA-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶ" + "ẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợ" + "ụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s]+$")]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      soDT: new FormControl(null, [Validators.required]),
+      message: new FormControl(null, [Validators.required])
+    })
+
+   }
 
   ngOnInit(): void {
   }
 
+  contactUs() {
+    const contactInfo = {
+      hoTen: this.contactForm.value.hoTen,
+      email: this.contactForm.value.email,
+      soDT: this.contactForm.value.soDT,
+      message: this.contactForm.value.message
+    }
+    console.log(contactInfo);
+    Swal.fire("Cảm ơn ! Chúng tôi sẽ liên hệ lại bạn sớm!")
+    this.contactForm.reset();
+  }
+
+  subscribed(subscribeForm: any) {
+    const subEmail = {
+      email: this.subscribeForm.value.email
+    }    
+    Swal.fire("Cảm ơn bạn đã đăng ký!");
+    this.subscribeForm.reset();
+  }
+
+  get email() { return this.subscribeForm.get("email")}
+  get hoTenCT() { return this.contactForm.get("hoTen")}
+  get emailCT() { return this.contactForm.get("email")}
+  get soDTCT() { return this.contactForm.get("soDT")}
+  get messageCT() { return this.contactForm.get("message")}
+  
 }
